9b1c12561cd1409105e2ef8fde5240c135058b58
Stijn van Dongen <micans@users.noreply.github.com>
2021-11-30 12:32:52 +0000

    dedupnonetoosoon

--- bin/incexp.pl
+++ bin/incexp.pl
@@ -4,6 +4,40 @@
 
 use warnings;
 use strict;
+use Getopt::Long;
+
+my @ARGV_COPY  = @ARGV;
+my $n_args = @ARGV;
+
+my $help     =  0;
+my $descent  =  0;
+my $progname = 'incexp.pl';
+
+
+sub help {
+   print <<EOH;
+Usage:
+   $progname [options]
+Options:
+-r              recurse
+EOH
+}
+
+if
+(! GetOptions
+   (  "h"      =>   \$help
+   ,  "r"      =>   \$descent
+   )
+)
+   {  print STDERR "option processing failed\n";
+      exit(1);
+   }
+
+if (!$n_args || $help) {
+   help();
+   exit 0;
+}
+
 
 while (<>) {
   if (/^\s*\((.*\.inc)\)\s*run/) {
@@ -12,9 +46,12 @@ while (<>) {
      my $filename = $file;
      $filename =~ s|(.*)/|.../|;
      print STDERR "-- expanding file $file\n";
-     print "% NOTE \"($filename) run\" expanded inline\n";
-     my $retval = system "cat $file";
+     print "% NOTE \"($filename) run\" expanded inline [\n";
+     my $retval = 0;
+     if ($descent) { $retval = system "incexp.pl -r $file"; }
+     else {          $retval = system "cat $file"; }
      die "No descent" if $retval;
+     print "% ] ($filename) run END\n";
   }
   else {
      print;
